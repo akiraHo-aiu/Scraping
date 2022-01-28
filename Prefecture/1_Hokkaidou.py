63df5fca231001ea85be7ab282babef66a2fe6c9
Akira HOSHIDA <h.akiraakita@gmail.com>
2022-01-28 18:29:51 +0900

    Comment and Manual

--- Prefecture/1_Hokkaidou.py
+++ Prefecture/1_Hokkaidou.py
@@ -3,7 +3,7 @@
 
 # In[5]:
 
-
+#ライブラリをロード
 from selenium import webdriver 
 import chromedriver_binary
 import os 
@@ -13,16 +13,19 @@ from selenium.webdriver.common.keys import Keys as keys
 
 # In[6]:
 
+#使用するブラウザーを指定　-- 都合が悪ければChrome以外のドライバーを用いればそれに応じたブラウザーも使用可能
 
 browser = webdriver.Chrome()
 
 
 # In[ ]:
 
+#使用したいデータ、表があるURLを読み込む
 browser.get('https://www.jma.go.jp/bosai/forecast/#area_type=offices&area_code=040000')
 
 
 # In[ ]:
+#find_element_by_id の””の中身を帰ることで取り出すデータを指定することができる。
 text = browser.find_element_by_id("short-table-container").text
 print(text)
 # %%
